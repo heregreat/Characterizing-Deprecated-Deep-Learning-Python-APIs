c8900e178e4ec665d5848c993f672f633ebd5034
Nian Liu <liuniangreat@gmail.com>
2021-05-16 18:09:44 -0400

    1

--- models-r1.13.0/official/mnist/mnist.py
+++ models-r1.13.0/official/mnist/mnist.py
@@ -95,7 +95,7 @@ def define_mnist_flags():
   flags_core.set_defaults(data_dir='/tmp/mnist_data',
                           model_dir='/tmp/mnist_model',
                           batch_size=100,
-                          train_epochs=1)
+                          train_epochs=40)
 
 
 def model_fn(features, labels, mode, params):
