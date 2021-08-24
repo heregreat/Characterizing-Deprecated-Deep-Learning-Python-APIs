976e58d8d8329da4ca5cfc591331e016ffbbe9b0
Nian Liu <liuniangreat@gmail.com>
2021-08-24 19:45:38 -0400

    update log

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ sudo apt-get update
 sudo apt-get install -y nvidia-docker2
 sudo systemctl restart docker
 sudo docker run --rm --gpus all nvidia/cuda:10.0-runtime nvidia-smi
-docker run --gpus all -it tensorflow/tensorflow:1.13.2-gpu bash
+docker run --gpus all -it -v /home/nianliu:/root tensorflow/tensorflow:1.13.2-gpu bash
 #pip3 install --user -r official/requirements.txt
